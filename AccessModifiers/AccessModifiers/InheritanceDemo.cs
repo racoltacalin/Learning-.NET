5d9e6f7172d30aad3fe0eee645ad8e5de94cde5a
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-11 15:00:19 +0200

    Explore access modifiers.

---
+++ AccessModifiers/AccessModifiers/InheritanceDemo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DemoLibrary;
+
+namespace AccessModifiers
+{
+    class InheritanceDemo: AccessDemo
+    {
+       private void Demo()
+        {
+            ProtectedDemo();
+        }
+    }
+}
---
+++ AccessModifiers/AccessModifiers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DemoLibrary;
+
+namespace AccessModifiers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var demo = new AccessDemo();
+            var bad = new BadClass();
+
+            bad.Age = 150;
+            bad._age = 150;
+            //bad._ssn;
+
+            demo.PublicDemo();
+        }
+
+        private static void SayHello()
+        {
+            Console.WriteLine("Hello user!");
+        }
+    }
+}
+
+ 
