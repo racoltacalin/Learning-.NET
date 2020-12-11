0e4f82da9e08c20a705f93f5972dd79c11301e10
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-11 15:03:04 +0200

    Learning interfaces in depth.

---
+++ Interface_Learning/DemoLibraries/DigitalProductModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLibraries
+{
+    public class DigitalProductModel : IProductModel, IDigitalProductModel
+    {
+        public string Title { get; set; }
+        public bool HasOrderBennCompleted { get; private set; }
+        public int TotalDownloadsLeft { get; private set; } = 5;
+
+        public void ShipItem(CustomerModel customer)
+        {
+            if (HasOrderBennCompleted == false)
+            {
+                Console.WriteLine($"Simulating emailing { Title } to { customer.EmailAddress}!");
+                TotalDownloadsLeft -= 1;
+                if (TotalDownloadsLeft < 1)
+                {
+                    HasOrderBennCompleted = true;
+                    TotalDownloadsLeft = 0;
+                }
+            }
+        }
+    }
+}
