4c1388e2e599a3fce30faf15763b60640984bbe8
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-13 12:53:28 +0200

    Understanding generics in C#

---
+++ Generics/Generics/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            list.Add(23);
+            list.Add("Hello");
+
+            List<int> ages = new List<int>();
+
+            ages.Add(23);
+
+            Console.WriteLine();
+            Console.WriteLine("Press enter to shut down...");
+            Console.ReadLine();
+        }
+
+        private static void DemonstrateTextFileStorage()
+        { 
+        }
+
+        private static void PopulateLists(List<Person> people, List<LogEntry> logs)
+        { 
+        }
+    }
+}
