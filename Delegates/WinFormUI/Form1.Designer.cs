5bf2876af665f42c22b9883ace6351c1cb48c87f
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-14 11:48:15 +0200

    Delegates in C#

--- Delegates/WinFormUI/Form1.Designer.cs
+++ Delegates/WinFormUI/Form1.Designer.cs
@@ -29,13 +29,97 @@ namespace WinFormUI
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.messageBoxDemoButton = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SubTotalTextBox = new System.Windows.Forms.TextBox();
+            this.TotalTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // messageBoxDemoButton
+            // 
+            this.messageBoxDemoButton.Location = new System.Drawing.Point(188, 225);
+            this.messageBoxDemoButton.Name = "messageBoxDemoButton";
+            this.messageBoxDemoButton.Size = new System.Drawing.Size(179, 123);
+            this.messageBoxDemoButton.TabIndex = 0;
+            this.messageBoxDemoButton.Text = "MessageBox Demo";
+            this.messageBoxDemoButton.UseVisualStyleBackColor = true;
+            this.messageBoxDemoButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(490, 225);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(179, 123);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "TextBox Demo";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // SubTotalTextBox
+            // 
+            this.SubTotalTextBox.Location = new System.Drawing.Point(397, 50);
+            this.SubTotalTextBox.Name = "SubTotalTextBox";
+            this.SubTotalTextBox.Size = new System.Drawing.Size(303, 23);
+            this.SubTotalTextBox.TabIndex = 2;
+            this.SubTotalTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // TotalTextBox
+            // 
+            this.TotalTextBox.Location = new System.Drawing.Point(397, 130);
+            this.TotalTextBox.Name = "TotalTextBox";
+            this.TotalTextBox.Size = new System.Drawing.Size(303, 23);
+            this.TotalTextBox.TabIndex = 3;
+            this.TotalTextBox.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(397, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 15);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Subtotal";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(397, 112);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(32, 15);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Total";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.TotalTextBox);
+            this.Controls.Add(this.SubTotalTextBox);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.messageBoxDemoButton);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button messageBoxDemoButton;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox SubTotalTextBox;
+        private System.Windows.Forms.TextBox TotalTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Delegates/WinFormUI/Form1.cs
+++ Delegates/WinFormUI/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DemoLibrary;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,10 +13,71 @@ namespace WinFormUI
 {
     public partial class Form1 : Form
     {
+        ShoppingCartModel cart = new ShoppingCartModel();
+
         public Form1()
         {
             InitializeComponent();
+            PopulateCartWithDemoData();
         }
 
+        private void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.513M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.843M });
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal(SubTotalAlert, CalculateLevelDiscount, PrintOutDiscountAlert);
+
+            MessageBox.Show($"The total is {total:C2}");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            decimal total = cart.GenerateTotal((subTotal) => SubTotalTextBox.Text = $"{subTotal:C2}",
+                (products, subTotal) => subTotal - (products.Count * 2),
+                (message) => { });
+
+            TotalTextBox.Text = $"{total:C2}";
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void PrintOutDiscountAlert(string message)
+        {
+            MessageBox.Show(message);
+        }
+
+        private void SubTotalAlert(decimal subTotal)
+        {
+            MessageBox.Show($"The subtotal is {subTotal:C2}");
+        }
+
+        private decimal CalculateLevelDiscount(List<ProductModel> products, decimal subTotal)
+        {
+            return subTotal - products.Count();
+        }
     }
 }
