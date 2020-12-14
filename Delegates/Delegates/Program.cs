233bd8dadbe9e2ebc66b4f992bcc573652cc45b5
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-14 10:48:56 +0200

    Delegates in C#

---
+++ Delegates/Delegates/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using DemoLibrary;
+using WinFormUI;
+
+namespace Delegates
+{
+    class Program
+    {
+        static ShoppingCartModel cart = new ShoppingCartModel();
+
+        static void Main(string[] args)
+        {
+            PopulateCartWithDemoData();
+
+            Console.WriteLine($"The total for the cart is { cart.GenerateTotal():C2}");
+            Console.WriteLine();
+            Console.Write("Please press any key to exit the application...");
+            Console.ReadKey();
+        }
+
+        private static void PopulateCartWithDemoData()
+        {
+            cart.Items.Add(new ProductModel { ItemName = "Cereal", Price = 3.63M });
+            cart.Items.Add(new ProductModel { ItemName = "Milk", Price = 2.95M });
+            cart.Items.Add(new ProductModel { ItemName = "Strawberries", Price = 7.513M });
+            cart.Items.Add(new ProductModel { ItemName = "Blueberries", Price = 8.843M });
+        }
+    }
+}
---
+++ Delegates/DemoLibrary/ProductModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DemoLibrary
+{
+    public class ProductModel
+    {
+        public string ItemName { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
