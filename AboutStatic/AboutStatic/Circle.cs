a0a655482258c18317bd35e606d22e7c7b3a41e7
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-16 12:26:24 +0200

    More about static in C#

---
+++ AboutStatic/AboutStatic/Circle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AboutStatic
+{
+    public class Circle
+    {
+        // Instance field = non-static fields
+        //float PI = 3.141F;
+        static float PI = 3.141F;
+        int Radius;
+
+        public Circle(int radius)
+        {
+            this.Radius = radius;
+        }
+
+
+
+        public float CalculateArea()
+        {
+            return Circle.PI * this.Radius * this.Radius; 
+        }
+    }
+}
---
+++ AboutStatic/AboutStatic/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AboutStatic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var circle1 = new Circle(5);
+            float area1 = circle1.CalculateArea();
+            Console.WriteLine("Area of the circle is: {0}", area1);
+
+            var circle2 = new Circle(6);
+            float area2 = circle2.CalculateArea();
+            Console.WriteLine("Area of the circle is: {0}", area2);
+
+            /*
+            for (int i = 0; i < 100; i++)
+            {
+                Circle objectA = new Circle(i);
+                float areaA = objectA.CalculateArea();
+                Console.WriteLine("Area of the circle[{0}] is: {1}", i, areaA);
+            }
+            */
+        }
+    }
+}
