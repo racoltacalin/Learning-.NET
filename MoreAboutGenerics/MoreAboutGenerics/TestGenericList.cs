37cafe3db30d5b02474cfb2bc5a2be6302712417
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-18 08:56:45 +0200

    More about Generics in C#

--- MoreAboutGenerics/MoreAboutGenerics/TestGenericList.cs
+++ MoreAboutGenerics/MoreAboutGenerics/TestGenericList.cs
@@ -7,6 +7,38 @@ namespace MoreAboutGenerics
         private class ExampleClass { }
         static void Main(string[] args)
         {
+            // int is the type argument
+
+            GenericList<int> list = new GenericList<int>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                list.AddHead(i);
+            }
+
+            foreach (int  i in list)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine("\n Done");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+            /* 
             // Declare a list of type int
             GenericList<int> list1 = new GenericList<int>();
             list1.Add(1);
@@ -21,6 +53,9 @@ namespace MoreAboutGenerics
             GenericList<ExampleClass> list3 = new GenericList<ExampleClass>();
             list3.Add(new ExampleClass());
             Console.WriteLine(list3);
+            */
+
+
         }
     }
 }
