015c34463290952cc5a1acda547793fcea5d2d58
racoltacalin <racolta.cvasile@hotmail.com>
2020-12-18 08:41:41 +0200

    More about Generics in C#

---
+++ MoreAboutGenerics/MoreAboutGenerics/GenericList.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoreAboutGenerics
+{
+    public class GenericList<T>
+    {
+        public void Add(T input)
+        {
+
+        }
+    }
+}
---
+++ MoreAboutGenerics/MoreAboutGenerics/TestGenericList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MoreAboutGenerics
+{
+    class TestGenericList
+    {
+        private class ExampleClass { }
+        static void Main(string[] args)
+        {
+            // Declare a list of type int
+            GenericList<int> list1 = new GenericList<int>();
+            list1.Add(1);
+            Console.WriteLine(list1);
+
+            // Declare a list of type string
+            GenericList<string> list2 = new GenericList<string>();
+            list2.Add("");
+            Console.WriteLine(list2);
+
+            // Declarea a list of type ExampleClass
+            GenericList<ExampleClass> list3 = new GenericList<ExampleClass>();
+            list3.Add(new ExampleClass());
+            Console.WriteLine(list3);
+        }
+    }
+}
