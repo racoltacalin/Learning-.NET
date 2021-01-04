a02492e7d444fdb5c061b4caca3c7bd18ad7f84c
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-04 14:32:39 +0200

    Console InputOutput

---
+++ ConsoleInputOutput/ConsoleInputOutput/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ConsoleInputOutput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Check_NumberIsPrime();
+            Console.ReadLine();
+        }
+
+        public static void Check_NumberIsPrime()
+        {
+            Console.Write("Enter a positive number: ");
+            int num = int.Parse(Console.ReadLine());
+            int divider = 2;
+            int maxDivider = (int)Math.Sqrt(num);
+            bool prime = true;
+            while (prime && (divider <= maxDivider))
+            {
+                if (num % divider == 0)
+                {
+                    prime = false;
+                }
+                divider++;
+            }
+            Console.WriteLine("Prime? " + prime);
+
+        }
+    }
+}
