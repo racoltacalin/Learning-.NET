30165eed73d29395edd879ca966d40a17a14cb54
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-04 09:50:38 +0200

    Operators And Expression Exercises

--- Operators_And_Expressions/Operators_And_Expressions/Program.cs
+++ Operators_And_Expressions/Operators_And_Expressions/Program.cs
@@ -7,8 +7,9 @@ namespace Operators_And_Expressions
         static void Main(string[] args)
         {
 
-            Check_Odd();
-            Check_Divisible();
+            //Check_Odd();
+            //Check_Divisible();
+            Check_ThirdDigit();
             Console.ReadLine();
         }
 
@@ -37,6 +38,26 @@ namespace Operators_And_Expressions
             userInput = Console.ReadLine();
             a = Convert.ToInt32(userInput);
             Console.WriteLine((a % 5 == 0) && (a % 7 == 0) ? "The number you've insert is divisible by both 5 and 7" : "The number you've insert is NOT divisible by both 5 and 7");
+
+        }
+
+        public static void Check_ThirdDigit()
+        {
+            int a;
+            int b;
+            string userInput;
+            Console.Write("Insert the number to looks if its third digit(right to left) is 7: ");
+            userInput = Console.ReadLine();
+            a = Convert.ToInt32(userInput);
+            b = a / 100;
+            if ( b % 10  == 7)
+            {
+                Console.WriteLine("The third digit right to left is 7!");
+            }
+            else
+            {
+                Console.WriteLine("The third digit right to left is NOT 7!");
+            }
 
         }
     }
