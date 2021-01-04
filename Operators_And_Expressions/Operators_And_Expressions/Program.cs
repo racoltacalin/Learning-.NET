13a467fe387d20ea84b4b9cf0e4678a1f53d4483
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-04 09:36:44 +0200

    Operators And Expression Exercises

---
+++ Operators_And_Expressions/Operators_And_Expressions/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Operators_And_Expressions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Check_Odd();
+            Check_Divisible();
+            Console.ReadLine();
+        }
+
+        public static void Check_Odd()
+        {
+            int a;
+            string userInput;
+            Console.Write("Insert the number you want to check if it is even or odd: ");
+            userInput = Console.ReadLine();
+            a = Convert.ToInt32(userInput);
+            if (a % 2 == 0)
+            {
+                Console.WriteLine($"The number {a} is even!");
+            }
+            else
+            {
+                Console.WriteLine($"The number {a} is odd!");
+            }
+        }
+
+        public static void Check_Divisible()
+        {
+            int a;
+            string userInput;
+            Console.Write("Insert the number you want to check if it is divisible by both 5 and 7: ");
+            userInput = Console.ReadLine();
+            a = Convert.ToInt32(userInput);
+            Console.WriteLine((a % 5 == 0) && (a % 7 == 0) ? "The number you've insert is divisible by both 5 and 7" : "The number you've insert is NOT divisible by both 5 and 7");
+
+        }
+    }
+}
