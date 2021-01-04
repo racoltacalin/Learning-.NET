51724a37fffa2e5c41fe8f0684dbc8518fe4708d
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-04 17:42:28 +0200

    Console Input/Output

--- ConsoleInputOutput/ConsoleInputOutput/Program.cs
+++ ConsoleInputOutput/ConsoleInputOutput/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Numerics;
 
 namespace ConsoleInputOutput
 {
@@ -6,7 +7,10 @@ namespace ConsoleInputOutput
     {
         static void Main(string[] args)
         {
-            Check_NumberIsPrime();
+            //Check_NumberIsPrime();
+            //Calculating_Factorial();
+            //Calculate_Factorial_Using_DoWhile();
+            Find_Product_InRange();
             Console.ReadLine();
         }
 
@@ -26,7 +30,60 @@ namespace ConsoleInputOutput
                 divider++;
             }
             Console.WriteLine("Prime? " + prime);
+        }
 
+        public static void Calculating_Factorial()
+        {
+            Console.Write("Please insert the number for calculate the factorial of it: ");
+            int n = int.Parse(Console.ReadLine());
+            decimal factorial = 1;
+            Console.Write($"{n}! = ");
+            while(true)
+            {
+                if ( n <= 1)
+                {
+                    break;
+                }
+                factorial *= n;
+                n--;
+            }
+            Console.Write(factorial);
+        }
+
+        public static void Calculate_Factorial_Using_DoWhile()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+            Console.Write($"{n}! = ");
+            //decimal factorial = 1;
+            BigInteger factorial = 1;
+            do
+            {
+                factorial *= n;
+                n--;
+            }
+            while (n > 0);
+            Console.WriteLine(factorial);
+        }
+
+        public static void Find_Product_InRange()
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            Console.Write("m = ");
+            int m = int.Parse(Console.ReadLine());
+
+            int num = n;
+            long product = 1;
+            do
+            {
+                product *= num;
+                num++;
+            }
+            while (num <= n);
+
+            Console.WriteLine("product[n...m] = " + product);
         }
     }
 }
