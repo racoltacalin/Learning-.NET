873c2e69ce0ca6674f760e101d3046be4c9a2ab8
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-06 19:49:23 +0200

    Some examples using methods in C#

--- MethodsInCsharp/MethodsInCsharp/Program.cs
+++ MethodsInCsharp/MethodsInCsharp/Program.cs
@@ -9,7 +9,26 @@ namespace MethodsInCsharp
             //GetRectangleArea(10, 20);
             //PrintTotalAmountForBooks(new decimal[] { 10, 10, 10 });
             //PrintSign(-5);
-            PrintMax(542, 642);
+            //PrintMax(542, 642);
+
+            /*int numberArg = 3;
+            PrintNumber(numberArg);
+            Console.WriteLine("In the Main() method numberArg is: " + numberArg);
+            */
+
+            /*
+            int[] arrArg = new int[] { 1, 2, 3, 4, 5 };
+
+            Console.Write("Before ModifyArray() the argument is: ");
+            PrintArray(arrArg);
+            // Modifying the array's argument
+            ModifyArray(arrArg);
+
+            Console.Write("After ModifyArray() the argument is: ");
+            PrintArray(arrArg);
+            */
+
+            
             Console.ReadLine();
         }
 
@@ -47,7 +66,7 @@ namespace MethodsInCsharp
         }
 
         static void PrintMax(float number1, float number2)
-        {
+        {   
             float max = number1;
 
             if (number2 > max)
@@ -55,6 +74,35 @@ namespace MethodsInCsharp
                 max = number2;
             }
             Console.WriteLine("Maximal number: " + max);
+        }
+
+        static void PrintNumber(int numberParam)
+        {
+            numberParam = 5;
+            Console.WriteLine("In PrintNumber() method, after " + "modification, numberParam is: {0}", numberParam);
+        }
+
+        static void ModifyArray(int[] arrParam)
+        {
+            arrParam[0] = 5;
+            Console.Write("In ModifyArray() the param is: ");
+            PrintArray(arrParam);
+        }
+
+        static void PrintArray(int[] arrParam)
+        {
+            Console.Write("[");
+            int length = arrParam.Length;
+
+            if (length > 0)
+            {
+                Console.Write(arrParam[0].ToString());
+                for (int i = 1; i < length; i++)
+                {
+                    Console.Write(", {0}", arrParam[i]);
+                }
+            }
+            Console.WriteLine("]");
         }
     }
 }
