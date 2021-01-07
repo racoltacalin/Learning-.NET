7120e57dac452c786857bbd6e70064f52e52192a
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-07 21:33:56 +0200

    Creating and using Objects

---
+++ CreatingAndUsingObjects/CreatingAndUsingObjects/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CreatingAndUsingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cat firstCat = new Cat();
+            firstCat.Name = "Tony";
+            firstCat.SayMiau();
+
+            Cat secondCat = new Cat("Pepy", "red");
+            secondCat.SayMiau();
+            Console.WriteLine("Cat {0} is {1}.", secondCat.Name, secondCat.Color);
+        }
+    }
+}
