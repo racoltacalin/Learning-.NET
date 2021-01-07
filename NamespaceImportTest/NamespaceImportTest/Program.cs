c25e7a11f2ce9a545822111dca7e8c7ab19aab91
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-08 01:21:57 +0200

    Namespace test program

---
+++ NamespaceImportTest/NamespaceImportTest/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace NamespaceImportTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.List<int> ints = new System.Collections.Generic.List<int>();
+            var doubles = new System.Collections.Generic.List<double>();
+
+            while (true)
+            {
+                int intResult;
+                double doubleResult;
+                Console.Write("Enteer an int or a double: ");
+                string input = Console.ReadLine();
+
+                if (int.TryParse(input, out intResult))
+                {
+                    ints.Add(intResult);
+                }
+                else if (double.TryParse(input, out doubleResult))
+                {
+                    doubles.Add(doubleResult);
+                }
+                else
+                {
+                    break;
+                }
+            }
+
+            Console.Write("You entered {0} ints:", ints.Count);
+            foreach (var i in ints)
+            {
+                Console.Write(" " + i);
+            }
+            Console.WriteLine();
+
+            Console.Write("You entered {0} doubles:", doubles.Count);
+            foreach (var d in doubles)
+            {
+                Console.Write(" " + d);
+            }
+            Console.WriteLine();
+        }
+    }
+}
