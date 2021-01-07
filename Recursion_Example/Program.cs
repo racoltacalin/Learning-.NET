3fc75bf397d523070f78c947a758547bd99ffde6
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-07 09:20:05 +0200

    Recursion Example in C#

---
+++ Recursion_Example/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Recursion_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CountOccurences(new int[] { 3, 2, 2, 5, 1, -8, 7, 2 });
+        }
+
+        static void CountOccurences(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int count = 0;
+                if (numbers[i] == numbers[i+1])
+                {
+                    count += 1;
+                }
+                Console.WriteLine("Count = {0}", count);
+            }
+        }
+    }
+}
