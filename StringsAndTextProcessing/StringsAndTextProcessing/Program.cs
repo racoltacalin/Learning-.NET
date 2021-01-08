9f26a352a3571be3f47dd33f97ef28aa04f36c45
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-08 21:47:26 +0200

    Strings And Text Processing

--- StringsAndTextProcessing/StringsAndTextProcessing/Program.cs
+++ StringsAndTextProcessing/StringsAndTextProcessing/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace StringsAndTextProcessing
 {
@@ -6,14 +7,41 @@ namespace StringsAndTextProcessing
     {
         static void Main(string[] args)
         {
+
+
+            string declared = "Intern pool";
+            string built = new StringBuilder("Intern pool").ToString();
+            string interned = string.Intern(built);
+            Console.WriteLine(object.ReferenceEquals(declared, built));
+            Console.WriteLine(object.ReferenceEquals(declared, interned));
+
+            /*
+            string alpha = "alpha";
+            string score1 = "sCorE";
+            string score2 = "score";
+            Console.WriteLine(string.Compare(alpha, score1, false));
+            Console.WriteLine(string.Compare(score1, score2, false));
+            Console.WriteLine(string.Compare(score1, score2, true));
+            Console.WriteLine(string.Compare(score1, score2, StringComparison.CurrentCultureIgnoreCase));
+            */
+
+            /*
+            string score = "sCore";
+            string scary = "scary";
+            Console.WriteLine(score.CompareTo(scary));
+            Console.WriteLine(scary.CompareTo(score));
+            Console.WriteLine(scary.CompareTo(scary));
+            */
+
+            /*
             string word1 = "C#";
             string word2 = "c#";
             Console.WriteLine(word1.Equals("C#"));
             Console.WriteLine(word1.Equals(word2));
             Console.WriteLine(word1 == "C#");
             Console.WriteLine(word1 == word2);
-
-
+            Console.WriteLine(word1.Equals(word2, StringComparison.CurrentCultureIgnoreCase));
+            */
 
             /*
             string message = "This is a sample string message.";
