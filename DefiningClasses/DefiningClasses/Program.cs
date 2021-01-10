d7d359593cfd921b8f68a87b4b54637b20e5944f
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-10 14:02:45 +0200

    Defininf Classes in depth

--- DefiningClasses/DefiningClasses/Program.cs
+++ DefiningClasses/DefiningClasses/Program.cs
@@ -6,7 +6,36 @@ namespace DefiningClasses
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CreateDogs();
+        }
+
+        static void CreateDogs()
+        {
+            string firstDogName = null;
+            Console.WriteLine("Enter the first dog name: ");
+            firstDogName = Console.ReadLine();
+
+            // Using a constructor to create a dog with specified name
+            Dog firstDog = new Dog(firstDogName);
+
+            // Using a constructor to create a dog wit a default name
+            Dog secondDog = new Dog();
+
+            Console.WriteLine("Enter second dog name: ");
+            string secondDogName = Console.ReadLine();
+
+            // Using property to set the name of the dog
+            secondDog.Name = secondDogName;
+
+            // Creating a dog with a default name
+            Dog thirdDog = new Dog();
+
+            Dog[] dogs = new Dog[] { firstDog, secondDog, thirdDog };
+
+            foreach (Dog dog in dogs)
+            {
+                dog.Bark();
+            }
         }
     }
 }
