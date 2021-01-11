cb063bf415099a31c472aeba3f612510ea480947
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-11 17:21:27 +0200

    Constructors in C# | In depth

---
+++ ConstructorsInCsharp/ConstructorsInCsharp/SystemInfo.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ConstructorsInCsharp
+{
+    public class SystemInfo
+    {
+        private static double version = 0.1;
+        private static string vendor = "Microsoft";
+
+        // The "version" static property
+        public static double Version
+        {
+            get { return version; }
+            set { version = value; }
+        }
+
+        // The "vendor" static property
+        public static string Vendor
+        {
+            get { return vendor; }
+            set { vendor = value; }
+        }
+
+        //... More (non)static code here ...
+    }
+}
