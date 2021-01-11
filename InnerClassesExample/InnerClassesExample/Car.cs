6fd0bc26a24073ca8ec0b67a72bdcba1fb71a27b
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-12 00:37:19 +0200

    Inner Classes Example

---
+++ InnerClassesExample/InnerClassesExample/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InnerClassesExample
+{
+    public class Car
+    {
+        Engine engine;
+
+        public Car()
+        {
+            engine = new Engine();
+            engine.horsePower = 2000;
+        }
+
+        public class Engine
+        {
+            public int horsePower;
+        }
+    }
+}
---
+++ InnerClassesExample/InnerClassesExample/OuterClass.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InnerClassesExample
+{
+    public class OuterClass
+    {
+        private string name;
+
+        private OuterClass(string name)
+        {
+            this.name = name;
+        }
+
+        private class NestedClass
+        {
+            private string name;
+            private OuterClass parent;
+
+            public NestedClass(OuterClass parent, string name)
+            {
+                this.parent = parent;
+                this.name = name;
+            }
+
+            public void PrintNames()
+            {
+                Console.WriteLine("Nested name: " + this.name);
+                Console.WriteLine("Outer name: " + this.parent.name);
+            }
+        }
+
+
+        static void Main()
+        {
+            OuterClass outerClass = new OuterClass("outer");
+            NestedClass nestedClass = new OuterClass.NestedClass(outerClass, "nested");
+            nestedClass.PrintNames();
+        }
+    }
+}
