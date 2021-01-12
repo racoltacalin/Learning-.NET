d06113967dfaa56662220cfa44dc4ba5ec6e7924
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-12 23:19:27 +0200

    Linear Data Structures Linked List Dynamic Implementation

---
+++ LinearDataStructuresLinkedListDynamic/LinearDataStructuresLinkedListDynamic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace LinearDataStructuresLinkedListDynamic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicList<string> shoppingList = new DynamicList<string>();
+
+            shoppingList.Add("Milk");
+            shoppingList.Remove("Milk"); // Empty list
+            shoppingList.Add("Honey");
+            shoppingList.Add("Olives");
+            shoppingList.Add("Water");
+            shoppingList[2] = "A lot of " + shoppingList[2];
+            shoppingList.Add("Fruits");
+            shoppingList.RemoveAt(0); // Removes "Honey" (first)
+            shoppingList.RemoveAt(2); // Removes "Fruits" (last)
+            shoppingList.Add(null);
+            shoppingList.Add("Beer");
+            shoppingList.Remove(null);
+            Console.WriteLine("We need to buy:");
+
+            for (int i = 0; i < shoppingList.Count; i++)
+            {
+                Console.WriteLine(" - " + shoppingList[i]);
+            }
+
+            Console.WriteLine("Position of 'Beer' = {0}", shoppingList.IndexOf("Beer"));
+            Console.WriteLine("Position of 'Water' = {0}", shoppingList.IndexOf("Water"));
+            Console.WriteLine("Do we have to buy Bread? " + shoppingList.Contains("Bread"));
+        }
+    }
+}
