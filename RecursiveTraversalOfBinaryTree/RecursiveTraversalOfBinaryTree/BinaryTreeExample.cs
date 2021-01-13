1f898e4a4ca5bee4cd774ac1dd04e7196721ff30
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-13 12:44:53 +0200

    Recursive Traversal of Binary Tree Example

---
+++ RecursiveTraversalOfBinaryTree/RecursiveTraversalOfBinaryTree/BinaryTreeExample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursiveTraversalOfBinaryTree
+{
+    public class BinaryTreeExample
+    {
+        static void Main()
+        {
+            // Create the binary tree from the sample
+            BinaryTree<int> binaryTree =
+                new BinaryTree<int>(14,
+                    new BinaryTree<int>(19,
+                        new BinaryTree<int>(23),
+                        new BinaryTree<int>(6,
+                            new BinaryTree<int>(10),
+                            new BinaryTree<int>(21))),
+                    new BinaryTree<int>(15,
+                        new BinaryTree<int>(3),
+                        null));
+            // Traverse and print the tree in in-order manner
+            binaryTree.PrintInOrder();
+            Console.WriteLine();
+        }
+    }
+}
