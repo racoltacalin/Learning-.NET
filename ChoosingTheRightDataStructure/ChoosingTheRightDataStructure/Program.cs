4747826845a9bf9870e82191656321436f161ccf
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-15 00:06:26 +0200

    How to choose the right Data Structure

--- ChoosingTheRightDataStructure/ChoosingTheRightDataStructure/Program.cs
+++ ChoosingTheRightDataStructure/ChoosingTheRightDataStructure/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 
 namespace ChoosingTheRightDataStructure
 {
@@ -9,11 +10,65 @@ namespace ChoosingTheRightDataStructure
         static string[] words = { "ocean", "beer", "money", "happiness" };
         static void Main(string[] args)
         {
-            GenerateSubset2();
+            PhoneBookFinder.ReadPhoneBook();
+            PhoneBookFinder.ProcessQueries();
 
+            //SortingPhoneBook();
+
+            //Student.ReadFile();
+
+            //GenerateSubset2();
             //GenerateSubset1();
+        }
 
-            Console.ReadLine();
+        public static void SortingPhoneBook()
+        {
+            // Read the file and build the phone book
+            SortedDictionary<string, SortedDictionary<string, string>> phonesByTown = new SortedDictionary<string, SortedDictionary<string, string>>();
+
+            string filePath = @"C:\Users\racol\OneDrive\Desktop\C# Learning\Learning_C#\Learning-.NET\ChoosingTheRightDataStructure\ChoosingTheRightDataStructure\PhoneBookFile.txt";
+            StreamReader reader = new StreamReader(filePath);
+
+            using (reader)
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+                    string[] entry = line.Split(new char[] { '|' });
+                    string name = entry[0].Trim();
+                    string town = entry[1].Trim();
+                    string phone = entry[2].Trim();
+
+                    SortedDictionary<string, string> phoneBook;
+                    if (! phonesByTown.TryGetValue(town, out phoneBook))
+                    {
+                        // This town is new. Create a phone book for it
+                        phoneBook = new SortedDictionary<string, string>();
+                        phonesByTown.Add(town, phoneBook);
+                    }
+                    phoneBook.Add(name, phone);
+                }
+            }
+
+            // Print the phone book by towns
+            foreach (string town in phonesByTown.Keys)
+            {
+                Console.WriteLine("Town " + town + ":");
+                SortedDictionary<string, string> phoneBook = phonesByTown[town];
+
+                foreach (var entry in phoneBook)
+                {
+                    string name = entry.Key;
+                    string phone = entry.Value;
+                    Console.WriteLine("\t{0} - {1}", name, phone);
+                }
+            }
+
+
         }
 
         public static void GenerateSubset1()
---
+++ ChoosingTheRightDataStructure/ChoosingTheRightDataStructure/Student.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.IO.Enumeration;
+using System.Text;
+
+namespace ChoosingTheRightDataStructure
+{
+    public class Student : IComparable<Student>
+    {
+        private string firstName;
+        private string lastName;
+
+        public Student(string firstName, string lastName)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+        }
+
+        public int CompareTo(Student student)
+        {
+            int result = lastName.CompareTo(student.lastName);
+            if (result == 0)
+            {
+                result = firstName.CompareTo(student.firstName);
+            }
+            return result;
+        }
+
+        public override string ToString()
+        {
+            return firstName + " " + lastName;
+        }
+
+        public static void ReadFile()
+        {
+            Dictionary<string, List<Student>> courses = new Dictionary<string, List<Student>>();
+
+            string filePath = @"C:\Users\racol\OneDrive\Desktop\C# Learning\Learning_C#\Learning-.NET\ChoosingTheRightDataStructure\ChoosingTheRightDataStructure\Students.txt";
+            StreamReader reader = new StreamReader(filePath);
+
+            using (reader)
+            {
+                while (true)
+                {
+                    string line = reader.ReadLine();
+                    if (line == null)
+                    {
+                        break;
+                    }
+
+                    string[] entry = line.Split(new char[] { '|' });
+                    string firstName = entry[0].Trim();
+                    string lastName = entry[1].Trim();
+                    string course = entry[2].Trim();
+                    List<Student> students;
+
+                    if (! courses.TryGetValue(course, out students))
+                    {
+                        // New course -> create a list of students for it
+                        students = new List<Student>();
+                        courses.Add(course, students);
+                    }
+                    Student student = new Student(firstName, lastName);
+                    students.Add(student);
+                }
+            }
+
+            // Print the courses and their students
+            foreach (string course in courses.Keys)
+            {
+                Console.WriteLine("Course " + course + ":");
+                List<Student> students = courses[course];
+                students.Sort();
+                foreach (Student student in students)
+                {
+                    Console.WriteLine("\t{0}", student);
+                }
+            }
+        }
+    }
+}
