5c0cf65825e67a1efd9e2e6fbc29115ea74da7b6
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-14 21:42:13 +0200

    Some example about sorting numbers

---
+++ SortingTenThousandNumbers/SortingTenThousandNumbers/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace SortingTenThousandNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10000];
+            Random rnd = new Random();
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = rnd.Next(2 * numbers.Length);
+
+                SortNumbers(numbers);
+                PrintNumbers(numbers);
+            }
+        }
+
+        static void SortNumbers(int[] numbers)
+        {
+            for (int i = 0; i < numbers.Length - 1; i++)
+            {
+                int minIndex = i;
+                for (int j = i + 1; j < numbers.Length; j++)
+                {
+                    if (numbers[j] < numbers[minIndex])
+                    {
+                        minIndex = j;
+                    }
+                }
+                int oldNumber = numbers[i];
+                numbers[i] = numbers[minIndex];
+                numbers[minIndex] = oldNumber;
+            }
+        }
+        static void PrintNumbers(int[] numbers)
+        {
+            Console.Write("[");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write(numbers[i]);
+                if (i < numbers.Length - 1)
+                {
+                    Console.Write(", ");
+                }
+            }
+            Console.WriteLine("]");
+        }
+    }
+}
