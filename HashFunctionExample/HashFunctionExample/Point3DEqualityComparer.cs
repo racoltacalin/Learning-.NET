9e95362d829c7b42ae7bfc31cfa4d5b23c65628b
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-14 10:43:11 +0200

    Implementing GetHashCode() - Example - Interface IEqualityComparer

---
+++ HashFunctionExample/HashFunctionExample/Point3DEqualityComparer.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace HashFunctionExample
+{
+    public class Point3DEqualityComparer : IEqualityComparer<Point3D>
+    {
+        public bool Equals(Point3D point1, Point3D point2)
+        {
+            if (point1 == point2)
+            {
+                return true;
+            }
+
+            if (point1 == null || point2 == null)
+            {
+                return false;
+            }
+
+            if (!point1.X.Equals(point2.X))
+            {
+                return false;
+            }
+
+            if (!point1.Y.Equals(point2.Y))
+            {
+                return false;
+            }
+
+            if (!point1.Z.Equals(point2.Z))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public int GetHashCode(Point3D obj)
+        {
+            Point3D point = obj as Point3D;
+            if (point == null)
+            {
+                return 0;
+            }
+
+            int prime = 83;
+            int result = 1;
+            unchecked
+            {
+                result = result * prime + point.X.GetHashCode();
+                result = result * prime + point.Y.GetHashCode();
+                result = result * prime + point.Z.GetHashCode();
+            }
+
+            return result;
+        }
+    }
+}
---
+++ HashFunctionExample/HashFunctionExample/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashFunctionExample
+{
+    class Program
+    {
+
+        static void Main()
+        {
+            IEqualityComparer<Point3D> comparer = new Point3DEqualityComparer();
+            Dictionary<Point3D, int> dict = new Dictionary<Point3D, int>(comparer);
+
+            dict[new Point3D(4, 2, 5)] = 5;
+            dict[new Point3D(1, 2, 3)] = 1;
+            dict[new Point3D(3, 1, -1)] = 3;
+            dict[new Point3D(1, 2, 3)] = 10;
+
+            foreach (var entry in dict)
+            {
+                Console.WriteLine("{0} --> {1}", entry.Key, entry.Value);
+            }
+
+        }
+
+
+
+        /*
+        public override unsafe int GetHashCode()
+        {
+            fixed (char* str = ((char*)this))
+            {
+                char* chPtr = str;
+                int num = 352654597;
+                int num2 = num;
+                int* numPtr = (int*)chPtr;
+
+                for (int i = this.Length; i > 0; i -= 4)
+                {
+                    num = (((num << 5) + num) + (num >> 27)) ^ numPtr[0];
+                    if (i <= 2)
+                    {
+                        break;
+                    }
+                    num2 = (((num2 << 5) + num2) + (num2 >> 27)) ^ numPtr[1];
+                    numPtr += 2;
+                }
+                return (num + (num2 * 1566083941));
+            }
+        }
+        */
+    }
+}
