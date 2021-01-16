99f4f5dfd59561ca8c8d791ed558e5e180024903
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-16 17:16:55 +0200

    Simple Unit Testing Example

---
+++ UnitTestingExample/UnitTestingExample/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace UnitTestingExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (Sum(new int[] { 1, 2 }) != 3)
+            {
+                throw new Exception("1 + 2 != 3");
+            }
+        }
+
+        static int Sum(int[] numbers)
+        {
+            int sum = numbers[0];
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                sum += numbers[i];
+            }
+            return sum;
+        }
+    }
+}
