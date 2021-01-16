c13bea8fae24778e4560fd95a22f13d879984f80
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-16 11:42:19 +0200

    Design Pattern - The Factory Method

---
+++ DesignPatterns_TheFactoryMethod/DesignPatterns_TheFactoryMethod/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace DesignPatterns_TheFactoryMethod
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IList<Image> images = new List<Image>();
+            images.Add(new JpegImage());
+            images.Add(new GifImage());
+            ImageCollection imageRepository = new ImageCollection(images);
+            Console.WriteLine(imageRepository.CreateThumbnails());
+        }
+    }
+}
---
+++ DesignPatterns_TheFactoryMethod/DesignPatterns_TheFactoryMethod/Thumbnail.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns_TheFactoryMethod
+{
+    public class Thumbnail
+    {
+        // ...
+    }
+
+    public interface Image
+    {
+        Thumbnail CreateThumbnail();
+    }
+
+    public class GifImage : Image
+    {
+        public Thumbnail GetThumbnail()
+        {
+            // ... Create a GIF thumbnail here ...
+            return gifThumbnail;
+        }
+    }
+
+    public class JpegImage : Image
+    {
+        public Thumbnail GetThumbnail()
+        {
+            //... Create a JPEG thumbnail here ...
+            return jpegThumbnail;
+        }
+    }
+
+    public class ImageCollection
+    {
+        private IList<Image> images;
+
+        public ImageCollection(IList<Image> images)
+        {
+            this.images = images;
+        }
+    }
+
+    public IList<Thumbnail> CreateThumbnails()
+    {
+        IList<Thumbnail> thumbnails = new List<Thumbnail>(images.Count);
+        foreach (Image thumb in images)
+        {
+            thumbnails.Add(thumb.CreateThumbnail());
+        }
+        return thumbnails;
+    }
+}
