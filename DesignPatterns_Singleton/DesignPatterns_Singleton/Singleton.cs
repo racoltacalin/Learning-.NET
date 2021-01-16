a6c1eff7291d006f08833f19630221b5a68a3a1b
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-16 11:29:32 +0200

    Design Patterns - The Singleton Design Pattern

---
+++ DesignPatterns_Singleton/DesignPatterns_Singleton/Singleton.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignPatterns_Singleton
+{
+    public class Singleton
+    {
+        // The single instance
+        private static Singleton instance;
+
+        // Initialize the single instance
+        static Singleton()
+        {
+            instance = new Singleton();
+        }
+
+        // The property for retrieving the single instance
+        public static Singleton Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        // Private constructor: protects against direct instantiation
+        private Singleton()
+        {
+
+        }
+    }
+}
