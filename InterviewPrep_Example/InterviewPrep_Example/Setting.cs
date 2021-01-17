cf1f30d947f84f29f2a362fc24269118009294f7
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-18 00:42:28 +0200

    Preparing for Interview - example

---
+++ InterviewPrep_Example/InterviewPrep_Example/Setting.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPrep_Example
+{
+    /// <summary>
+    /// Keyword static
+    /// </summary>
+    public static class Setting
+    {
+    }
+}
