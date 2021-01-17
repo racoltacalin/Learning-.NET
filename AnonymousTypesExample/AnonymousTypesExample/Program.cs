1e37cae47cd6fe864038b5accc77159429e73545
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-17 09:32:20 +0200

    Anonymous Types in C#

---
+++ AnonymousTypesExample/AnonymousTypesExample/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace AnonymousTypesExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myCar = new { Color = "Red", Brand = "BMW", Speed = 180 };
+
+            Console.WriteLine("My car is a {0} : {1}.", myCar.Color, myCar.Brand);
+            Console.WriteLine("It runs {0} km/h", myCar.Speed);
+            Console.WriteLine();
+
+            var arr = new[]
+            {
+                new {X = 3, Y = 5},
+                new {X = 1, Y = 2 },
+                new {X = 0, Y = 8}
+            };
+            foreach (var item in arr)
+            {
+                Console.WriteLine(item.ToString());
+            }
+        }
+    }
+}
