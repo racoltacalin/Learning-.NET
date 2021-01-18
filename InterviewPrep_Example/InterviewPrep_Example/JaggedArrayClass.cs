3f7eecff4c46fd257cfc71d93cee3f9b0263b314
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-18 18:33:30 +0200

    Preparing for Interview - example

---
+++ InterviewPrep_Example/InterviewPrep_Example/JaggedArrayClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPrep_Example
+{
+    public class JaggedArrayClass
+    {
+        public void ShowJaggedArray()
+        {
+            int[][] jaggedArray = new int[2][];
+            jaggedArray[0] = new int[3] { 1, 2, 3 };
+            jaggedArray[1] = new int[2] { 0, 1 };
+        }
+    }
+}
---
+++ InterviewPrep_Example/InterviewPrep_Example/SomeClassAnonymousTypes.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterviewPrep_Example
+{
+    public class SomeClassAnonymousTypes
+    {
+        public void Print()
+        {
+            var anonymousData = new
+            {
+                FirsName = "John",
+                SurName = "lastname"
+            };
+            Console.WriteLine("First Name: " + anonymousData.FirsName);
+        }
+    }
+}
