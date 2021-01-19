0918881e3fbda71f02c96cb8da973227c4c7874a
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-19 21:44:46 +0200

    Example of struct in C#

---
+++ StructExample/StructExample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace StructExample
+{
+    class Program
+    {
+        struct Point
+        {
+            public int x, y;
+        }
+
+        struct Color
+        {
+            public byte red;
+            public byte green;
+            public byte blue;
+        }
+
+        struct Square
+        {
+            public Point location;
+            public int size;
+            public Color borderColor;
+            public Color sufaceColor;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
