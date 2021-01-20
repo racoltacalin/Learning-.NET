c621b7ae747c4e2a4bbcffb1f97097dd271c6fff
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-20 14:14:56 +0200

    Problems for Interview

--- ProblemsForInterview/ProblemsForInterview/Program.cs
+++ ProblemsForInterview/ProblemsForInterview/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace ProblemsForInterview
 {
@@ -9,8 +11,13 @@ namespace ProblemsForInterview
             Console.Write("n = ");
             int n = int.Parse(Console.ReadLine());
 
-            long result = Fibonacci(n);
-            Console.WriteLine("Fib({0}) = {1}", n, result);
+            long resultFibonacci = Fibonacci(n);
+            Console.WriteLine("Fib({0}) = {1}", n, resultFibonacci);
+            Console.WriteLine();
+
+            var integerList = GetIntegers();
+            FizzBuzz(integerList);
+
         }
 
         static long Fibonacci(int n)
@@ -26,6 +33,41 @@ namespace ProblemsForInterview
                 fnMinus1 = fn;
             }
             return fn;
+        }
+
+        public static List<int> GetIntegers()
+        {
+            var integerList = new List<int>();
+
+            for (int i = 0; i <= 100; i++)
+            {
+                integerList.Add(i);
+            }
+
+            return integerList;
+        }
+
+        public static void FizzBuzz(List<int> integerList)
+        {
+            foreach (var item in integerList)
+            {
+                if (item % 3 == 0 && item % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (item % 3 == 0)
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else if (item % 5 == 0)
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else
+                {
+                    Console.WriteLine(item);
+                }
+            }
         }
     }
 }
