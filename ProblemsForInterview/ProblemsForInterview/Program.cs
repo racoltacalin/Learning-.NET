b3824fb6510dd85e901abe588b2143904c19af94
racoltacalin <racolta.cvasile@hotmail.com>
2021-01-20 13:54:59 +0200

    Problems for Interview

---
+++ ProblemsForInterview/ProblemsForInterview/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace ProblemsForInterview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n = ");
+            int n = int.Parse(Console.ReadLine());
+
+            long result = Fibonacci(n);
+            Console.WriteLine("Fib({0}) = {1}", n, result);
+        }
+
+        static long Fibonacci(int n)
+        {
+            long fn = 0;
+            long fnMinus1 = 1;
+            long fnMinus2 = 1;
+
+            for (int i = 2; i < n; i++)
+            {
+                fn = fnMinus1 + fnMinus2;
+                fnMinus2 = fnMinus1;
+                fnMinus1 = fn;
+            }
+            return fn;
+        }
+    }
+}
